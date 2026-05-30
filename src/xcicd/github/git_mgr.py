3c231c47620aba9373d5e817c2d7296c6627b46b
Robert King <varitonium@gmail.com>
2026-05-30 22:57:49 +0200

    XCI-3: upe-GitMgr impl & xcicd-bdeleter script

--- src/xcicd/github/git_mgr.py
+++ src/xcicd/github/git_mgr.py
@@ -7,16 +7,15 @@ from xcicd.linux.scall import execute_cmd
 class GitMgr:
     """Manage git actions operation."""
 
-    def __init__(self, _repo: str, _current_branch: str, _token: str) -> None:
+    def __init__(self, _repo: str, _token: str) -> None:
         """Initialize git manager object."""
         self.repo_ = _repo
-        self.cbranch_ = _current_branch
         self.token_ = _token
 
-    def remove(self) -> None:
+    def remove_branch(self,current_branch:str) -> None:
         """Remove current branch."""
         # create api delete method 
-        delete_api_method = ApiMgr.create_delete_api_method(self.repo_, self.cbranch_)
+        delete_api_method = ApiMgr.create_delete_api_method(self.repo_, _current_branch)
         # create curl command 
         curl_cmd = CurlMgr.create_curl_cmd_with_token("DELETE", self.token_, delete_api_method)
         try:
