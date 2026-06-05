b2067b5e9f79d59a7bc042cef7e02c792929d16b
Robert King <varitonium@gmail.com>
2026-06-05 23:28:33 +0200

    XCI-6: upe-script compliance with python practices

--- scripts/xcicd-bscanner.py
+++ scripts/xcicd-bscanner.py
@@ -1,22 +1,17 @@
 
+#!/usr/bin/env python3
+
 import sys
 
-from xcicd.artifact.branch import Branch
+from xcicd.bscanner import main
 
-def main(args:list[str]) -> None:
-    current_branch_name = args[1]
-    last_commit_message = args[2]
-
-    branch = Branch(current_branch_name)
-    if branch.is_valid() == False:
-        raise RuntimeError("invalid branch name : "+current_branch_name)
-       
 if __name__ == "__main__":
-    
-    if len(sys.argv) < 2:
-        raise RuntimeError("missing arguments: \n xcicd-bscanner \
-                           CURRENT_BRANCH_NAME LAST_COMMIT")
+    if len(sys.argv) < 3:
+        raise RuntimeError(
+            "missing arguments:\n"
+            "xcicd-bscanner CURRENT_BRANCH_NAME LAST_COMMIT"
+        )
     try:
         main(sys.argv)
     except Exception as e:
-        raise RuntimeError(e.message)
+        raise RuntimeError(e)
